5aee866b1d5c9faa451962acb68b21671745d1ea
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-02-11 22:17:03 +0200

    implement SignUp and Login fragments

--- app/src/main/java/com/example/foodplanner/ConnectOnline/View/LoginFragment.java
+++ app/src/main/java/com/example/foodplanner/ConnectOnline/View/LoginFragment.java
@@ -9,18 +9,36 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.foodplanner.HomeScreen.View.HomeActivity;
 import com.example.foodplanner.R;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginFragment extends Fragment {
 
+    static final String TAG = "Login";
+    EditText emailLogin;
+    EditText passwordLogin;
+    Button btnLogin;
+
+//    Button btnGoogle;
+//    GoogleSignInClient gClient;
+//    GoogleSignInOptions gOptions;
+
+    FirebaseAuth mAuth;
     Button btnGuest;
     TextView signUp;
 
@@ -66,6 +84,51 @@ public class LoginFragment extends Fragment {
             }
         });
 
+
+        emailLogin=view.findViewById(R.id.Login_Email);
+        passwordLogin=view.findViewById(R.id.Login_Password);
+        btnLogin=view.findViewById(R.id.btn_login);
+        //btnGoogle=findViewById(R.id.btn_Google);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String user = emailLogin.getText().toString();
+
+                String pass = passwordLogin.getText().toString();
+                if(!user.isEmpty() && Patterns.EMAIL_ADDRESS
+                        .matcher(user).matches()){
+                    if(!pass.isEmpty()){
+                        mAuth.signInWithEmailAndPassword(user, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(startActivity, "Login Successful",
+                                                Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(startActivity, HomeActivity.class));
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(startActivity, "Login Failed",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }else{
+                        passwordLogin.setError("pass cant be empty");
+                    }
+
+                } else if (user.isEmpty()) {
+                    emailLogin.setError("email cant be empty");
+                }else{
+                    emailLogin.setError("enter valid email");
+                }
+            }
+        });
 
 
 
