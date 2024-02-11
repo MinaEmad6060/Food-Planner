8840698476acaa62c51afa93e1190c8a56f4192c
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-02-11 22:17:03 +0200

    implement SignUp and Login fragments

--- app/src/main/java/com/example/foodplanner/ConnectOnline/View/SignUpFragment.java
+++ app/src/main/java/com/example/foodplanner/ConnectOnline/View/SignUpFragment.java
@@ -1,5 +1,6 @@
 package com.example.foodplanner.ConnectOnline.View;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,12 +13,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.foodplanner.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpFragment extends Fragment {
+    private EditText email;
+    private EditText password;
 
-
+    private FirebaseAuth mAuth;
+    StartActivity startActivity;
     Button btnSignUp;
 //    Button btnGoogleSignUp;
 
@@ -42,5 +52,40 @@ public class SignUpFragment extends Fragment {
             NavDirections action = SignUpFragmentDirections.actionSignUpFragmentToLoginFragment();
             Navigation.findNavController(v).navigate(action);
         });
+
+        startActivity = (StartActivity) getActivity();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email=view.findViewById(R.id.SignUP_Email);
+        password=view.findViewById(R.id.SignUP_Password);
+        btnSignUp=view.findViewById(R.id.btn_signup);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                String user = email.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+
+                if (user.isEmpty()){
+                    email.setError("Email cannot be empty");
+                }else if (pass.isEmpty()) {
+                    password.setError("Password cannot be empty");
+                }else{
+                    mAuth.createUserWithEmailAndPassword(user, pass)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(startActivity, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                                        NavDirections action = SignUpFragmentDirections.actionSignUpFragmentToLoginFragment();
+                                        Navigation.findNavController(view).navigate(action); }
+                                    else {
+                                        Toast.makeText(startActivity, "SignUp Failed", Integer.parseInt(task.getException().getMessage())).show();
+                                    }
+                                }
+                            });
+                }}});
+
+
     }
 }
