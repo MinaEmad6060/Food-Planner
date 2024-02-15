cbb1b8ab55377355e76283af99d0c47320af271f
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-02-15 20:45:40 +0200

    merge test

--- app/src/main/java/com/example/foodplanner/Favourate/View/FavouriteFragment.java
+++ app/src/main/java/com/example/foodplanner/Favourate/View/FavouriteFragment.java
@@ -42,7 +42,6 @@ public class FavouriteFragment extends Fragment implements InterFavProductsView{
 
     InterFavMealsPresenter interFavMealsPresenter;
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
