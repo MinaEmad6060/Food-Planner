9b58398d6b6686f08017ef64e33daf7538d6086d
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-02-16 01:13:08 +0200

    merge test

--- app/src/main/java/com/example/foodplanner/Search/View/SearchFragment.java
+++ app/src/main/java/com/example/foodplanner/Search/View/SearchFragment.java
@@ -186,6 +186,7 @@ public class SearchFragment extends Fragment implements SearchViewInter{
                 err-> Log.i(TAG, "error: ")
                 );
     }
+
     @Override
     public void showSearchMeals(List<Meal> meals) {
         searchMeals=meals;
