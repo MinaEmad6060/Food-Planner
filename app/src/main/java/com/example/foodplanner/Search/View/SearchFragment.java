616474115e40165bed5ef683b1261d0fe039e4e9
minaemad6060@gmail.com <minaemad6060@gmail.com>
2024-02-15 05:13:05 +0200

    edit search by Meal name

--- app/src/main/java/com/example/foodplanner/Search/View/SearchFragment.java
+++ app/src/main/java/com/example/foodplanner/Search/View/SearchFragment.java
@@ -114,8 +114,6 @@ public class SearchFragment extends Fragment implements SearchViewInter{
             @Override
             public void onClick(View v) {
                 searchFragmentPresenterInter.getAllCategoriesPres();
-//                NavDirections action = SearchFragmentDirections.actionSearchFragmentToCategoryFragment();
-//                Navigation.findNavController(v).navigate(action);
             }
         });
 
